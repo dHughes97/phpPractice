3ed7ba26eb71cae4176ed68b0bc143b257469c9e
GoldenKunai <dhhollywood273@gmail.com>
2023-10-13 12:26:15 -0500

    finished functions

--- Beginner/functions.php
+++ Beginner/functions.php
@@ -6,6 +6,31 @@
     <title>Functions</title>
 </head>
 <body>
-    
+    <form method="post">
+        <input type="text" name="input">
+        <input type="submit" value="Submit">
+    </form>
+    <br>
+
+    <?php
+        // checks if the POST has been submitted
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // get the user input from the form
+            $userIO = $_POST["input"]; // retrieves the user input from the input field
+
+            // create the function sayHi
+            function sayHi() {
+                echo "Hello User! \n\n";
+            }
+
+            function userType($userIO) {
+                echo "Type whatever you want here: $userIO \n";
+            }
+
+            sayHi();
+            userType($userIO); // use the correct variable name here
+
+        }
+    ?>
 </body>
 </html>
